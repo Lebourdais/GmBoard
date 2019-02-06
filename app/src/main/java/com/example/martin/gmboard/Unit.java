3639a49e66ded21381ef8a6015d137fc63839e57
FernandesSimon <simon.fernandes.etu@univ-lemans.fr>
2019-02-06 20:30:20 +0100

    Modification Layout création d'unité Sauvegarde de l'unité

--- app/src/main/java/com/example/martin/gmboard/Unit.java
+++ app/src/main/java/com/example/martin/gmboard/Unit.java
@@ -1,5 +1,13 @@
 package com.example.martin.gmboard;
 
+import android.content.Context;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Hashtable;
 
 public class Unit {
@@ -10,9 +18,11 @@ public class Unit {
     private int def;
     private String notes;
     private Hashtable<String, Integer> stats;
+    private Context context;
+
 
     // To create a new Unit
-    public Unit (String pName, int pMaxHP, int pAtk, int pDef, String pNotes, Hashtable<String, Integer> pStats) {
+    public Unit (String pName, int pMaxHP, int pAtk, int pDef, String pNotes, Hashtable<String, Integer> pStats, Context pContext) {
         this.name = pName;
         this.maxHP = pMaxHP;
         this.currentHP = pMaxHP;
@@ -20,6 +30,7 @@ public class Unit {
         this.def = pDef;
         this.notes = pNotes;
         this.stats = pStats;
+        this.context = pContext;
     }
 
     // To load a previously created Unit
@@ -53,8 +64,43 @@ public class Unit {
         this.notes = newNotes;
     }
 
-    public void editStats (String newStats){
-        this.notes = newStats;
+    public void editStats (Hashtable<String, Integer> newStats){
+        this.stats = newStats;
+    }
+
+    public void saveToFile (Context context, String fileName){
+        JSONObject newUnit = new JSONObject();
+        try {
+            newUnit.put("name", name);
+            newUnit.put("maxHP", maxHP);
+            newUnit.put("currentHP", currentHP);
+            newUnit.put("attack", atk);
+            newUnit.put("defense", def);
+            newUnit.put("notes", notes);
+            newUnit.put("stats", stats);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        // Lit le fichier json existant
+        String jsonText = readText(context, R.raw.unitStorage);
+        JSONObject jsonRoot = new JSONObject(jsonText);
+
+        JSONArray jsonArray = new JSONArray();
+
+        jsonArray.put(jsonRoot);
+        jsonArray.put(newUnit);
+    }
+
+    private static String readText(Context context, int resId) throws IOException {
+        InputStream is = context.getResources().openRawResource(resId);
+        BufferedReader br= new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb= new StringBuilder();
+        String s = null;
+        while((  s = br.readLine())!=null) {
+            sb.append(s);
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
 }
---
+++ app/src/main/java/com/example/martin/gmboard/UnitUi.java
@@ -0,0 +1,56 @@
+package com.example.martin.gmboard;
+
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.Hashtable;
+
+public class UnitUi extends AppCompatActivity {
+
+    private static Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        context = getApplicationContext();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.content_unit_creation);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    public void onClick(View v) {
+
+        String name = ((EditText)findViewById(R.id.unitName)).getText().toString();
+        String notes = ((EditText)findViewById(R.id.unitNotes)).getText().toString();
+        int maxHP = Integer.parseInt(((EditText)findViewById(R.id.unitMaxHP)).getText().toString());
+        int attack = Integer.parseInt(((EditText)findViewById(R.id.unitAttack)).getText().toString());
+        int defense = Integer.parseInt(((EditText)findViewById(R.id.unitDefense)).getText().toString());
+        final int strength = Integer.parseInt(((EditText)findViewById(R.id.unitStrength)).getText().toString());
+        final int dexterity = Integer.parseInt(((EditText)findViewById(R.id.unitDexterity)).getText().toString());
+        final int constitution = Integer.parseInt(((EditText)findViewById(R.id.unitConstitution)).getText().toString());
+        final int intelligence = Integer.parseInt(((EditText)findViewById(R.id.unitIntelligence)).getText().toString());
+        final int wisdom = Integer.parseInt(((EditText)findViewById(R.id.unitWisdom)).getText().toString());
+        final int charisma = Integer.parseInt(((EditText)findViewById(R.id.unitCharisma)).getText().toString());
+
+        Hashtable stats = new Hashtable<String, Integer>() {{ put("STR",strength);
+                                                            put("DEX", dexterity);
+                                                            put("CON", constitution);
+                                                            put("INT", intelligence);
+                                                            put("WIS", wisdom);
+                                                            put("CHA", charisma);}};
+
+        Unit unit = new Unit(name, maxHP, attack, defense, notes, stats, context);
+
+        unit.saveToFile(context, "unitStorage.json");
+
+    }
+
+}
